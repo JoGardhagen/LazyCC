297aa36465d144d6a0e8c6053753986bd4045429
Joakim <superapa.spanx@gmail.com>
2026-08-09 15:23:13 +0200

    first push

---
+++ LazyCC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LazyCC
+//
+//  Created by Joakim Gårdhagen on 2026-08-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Lazy Color Code")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LazyCC/LazyCCApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyCCApp.swift
+//  LazyCC
+//
+//  Created by Joakim Gårdhagen on 2026-08-09.
+//
+
+import SwiftUI
+
+@main
+struct LazyCCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
